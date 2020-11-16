edfffa63bdfe763196e1c235e7d8ff19e0c8d01f
Maxim Zvenigorodsky <zaqhaha@yandex.ru>
2020-11-16 22:21:38 +0300

    Finished registration

--- iConnect/View Controllers/ViewController.swift
+++ iConnect/View Controllers/ViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 import FirebaseDatabase
-
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
     
@@ -19,11 +23,59 @@ class ViewController: UIViewController {
         
         setUpElements()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setUpVideo()
+    }
 
     func setUpElements() {
         Utilities.styleFieldButton(signUpButton)
         Utilities.styleHollowButton(loginButton)
     }
-
+    
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "bg2", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player iyem
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+//        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width:
+//                                            self.view.frame.size.width*4, height:
+//                                            self.view.frame.size.height)
+        
+        videoPlayerLayer?.frame = CGRect(
+                                        x: -self.view.frame.size.width*1.5, y: 0,
+                                        width: self.view.frame.size.width*4,
+                                        height: self.view.frame.size.height
+                                        )
+        
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+        
+        videoPlayer!.play()
+        
+    }
+    
 }
 
